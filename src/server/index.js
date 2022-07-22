1944f1593686d3a58ad6aa90eb93e568ebade511
Povilas Liniauskas <povilas.liniauskas@paysolut.com>
2022-07-22 09:18:53 +0300

    added api request to ../rovers

--- src/server/index.js
+++ src/server/index.js
@@ -1,28 +1,41 @@
-require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const fetch = require('node-fetch')
-const path = require('path')
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const fetch = require("node-fetch");
+const path = require("path");
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+const marsPhotosApi = "https://api.nasa.gov/mars-photos/api/v1/rovers";
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use('/', express.static(path.join(__dirname, '../public')))
+app.use("/", express.static(path.join(__dirname, "../public")));
 
-// your API calls
+// get rover name
+app.get("get rover name", async (req, res) => {
+  try {
+    let rover = await fetch(
+      `${marsPhotosApi}api_key=${process.env.API_KEY}`
+    ).then((res) => res.json());
+    console.log(rover);
+    //res.send({ image });
+  } catch (err) {
+    console.log("error:", err);
+  }
+});
 
 // example API call
-app.get('/apod', async (req, res) => {
-    try {
-        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
-            .then(res => res.json())
-        res.send({ image })
-    } catch (err) {
-        console.log('error:', err);
-    }
-})
+app.get("/apod", async (req, res) => {
+  try {
+    let image = await fetch(
+      `https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`
+    ).then((res) => res.json());
+    res.send({ image });
+  } catch (err) {
+    console.log("error:", err);
+  }
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
