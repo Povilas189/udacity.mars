b53f87c826c2bcb6fa54323af32d082ddf1a0e62
Povilas Liniauskas <povilas.liniauskas@paysolut.com>
2022-07-25 09:54:11 +0300

    banner chainge

--- src/public/client.js
+++ src/public/client.js
@@ -23,25 +23,13 @@ const App = (state) => {
   return `
         <header>
          <div class="banner">
-        <img class="banner-img" src= ${ImageOfTheDay(apod)}/>
+         ${ImageOfTheDay(apod)}
         <h1 class="banner-text">Photo of day</h1>
         </div>
+            ${Nav(roverNames, selectedRover)}
         </header>
         <main>
-            ${Greeting(store.user.name)}
-            <section>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
-               
-            </section>
+         ${RoverData(rovers, selectedRover, photos)}
         </main>
         <footer>
          <h6>
@@ -93,7 +81,6 @@ const ImageOfTheDay = (apod) => {
   } else {
     return `
             <img src="${apod.image.url}" height="350px" width="100%" />
-            <p>${apod.image.explanation}</p>
         `;
   }
 };
