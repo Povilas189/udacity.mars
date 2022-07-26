6d8aaf72e128eaf262e543e0522c8c07f42012c4
Povilas189 <96108280+Povilas189@users.noreply.github.com>
2022-07-26 06:05:41 +0300

    fix after review

--- src/server/index.js
+++ src/server/index.js
@@ -14,18 +14,26 @@ app.use("/", express.static(path.join(__dirname, "../public")));
 
 // your API calls
 app.get("/getRoverName/:rover_name", async (req, res) => {
-  let rover = await fetch(`
+  try {
+  const rover = await fetch(`
             ${rover_endpoint}/manifests/${req.params.rover_name}?api_key=${process.env.API_KEY}
         `).then((res) => res.json());
   res.send(rover);
+        } catch (err) {
+        console.log('error: ', err)
+    }
 });
 
 app.get("/getRoverCamerasAndPhotosInfo/:rover_name", async (req, res) => {
   const { rover_name } = req.params;
+  try{
   let rover_photos = await fetch(
     `${rover_endpoint}/rovers/${rover_name}/latest_photos?api_key=${process.env.API_KEY}
         `
   ).then((res) => res.json());
   res.send(rover_photos);
+        } catch (err) {
+        console.log('error: ', err)
+    }
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
